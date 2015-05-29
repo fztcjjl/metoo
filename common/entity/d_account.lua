b96f02a90a2d335a0f260f3727339680a7dc27a7
fztcjjl <fztcjjl@gmail.com>
2015-05-29 05:16:06 -0700

    delete unused entity

--- common/entity/d_account.lua
+++
@@ -1,9 +0,0 @@
-require "CommonEntity"
-
-local EntityType = class(CommonEntity)
-
-function EntityType:ctor()
-	self.tbname = "d_account"
-end
-
-return EntityType.new()
--- common/entity/d_shipcanvas.lua
+++
@@ -1,9 +0,0 @@
-require "UserMultiEntity"
-
-local EntityType = class(UserMultiEntity)
-
-function EntityType:ctor()
-	self.tbname = "d_shipcanvas"
-end
-
-return EntityType.new()
