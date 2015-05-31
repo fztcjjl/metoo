ea0c3adcfbdda2055e630ce57563e6b7f31a11aa
fztcjjl <fztcjjl@gmail.com>
2015-05-31 14:06:06 -0700

    update LOG_XXXX for print filename and line

--- global/util.lua
+++ global/util.lua
@@ -9,26 +9,46 @@ end
 
 function LOG_DEBUG(fmt, ...)
 	local msg = string.format(fmt, ...)
+	local info = debug.getinfo(2)
+	if info then
+		msg = string.format("[%s:%d] %s", info.short_src, info.currentline, msg)
+	end
 	skynet.send("log", "lua", "debug", SERVICE_NAME, msg)
 end
 
 function LOG_INFO(fmt, ...)
 	local msg = string.format(fmt, ...)
+	local info = debug.getinfo(2)
+	if info then
+		msg = string.format("[%s:%d] %s", info.short_src, info.currentline, msg)
+	end
 	skynet.send("log", "lua", "info", SERVICE_NAME, msg)
 end
 
 function LOG_WARNING(fmt, ...)
 	local msg = string.format(fmt, ...)
+	local info = debug.getinfo(2)
+	if info then
+		msg = string.format("[%s:%d] %s", info.short_src, info.currentline, msg)
+	end
 	skynet.send("log", "lua", "warning", SERVICE_NAME, msg)
 end
 
 function LOG_ERROR(fmt, ...)
 	local msg = string.format(fmt, ...)
+	local info = debug.getinfo(2)
+	if info then
+		msg = string.format("[%s:%d] %s", info.short_src, info.currentline, msg)
+	end
 	skynet.send("log", "lua", "error", SERVICE_NAME, msg)
 end
 
 function LOG_FATAL(fmt, ...)
 	local msg = string.format(fmt, ...)
+	local info = debug.getinfo(2)
+	if info then
+		msg = string.format("[%s:%d] %s", info.short_src, info.currentline, msg)
+	end
 	skynet.send("log", "lua", "fatal", SERVICE_NAME, msg)
 end
 
